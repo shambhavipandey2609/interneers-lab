97dd29f05bab40fc3f652a19ee35150f35ae6867
Shambhavi Pandey <pandeyshambhavi2609@gmail.com>
2025-03-02 11:40:29 +0530

    Added new API endpoint: /Bye with customizable greetings

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -9,11 +9,19 @@ def hello_name(request):
     """
     # Get 'name' from the query string, default to 'World' if missing
     name = request.GET.get("name", "World")
-    return JsonResponse({"message": f"Hello, {name}!"})
+    greeting =request.GET.get("greeting","Hello")
+    return JsonResponse({"message": f"{greeting}, {name}!"})
+
+def Bye_name(request):
+    name = request.GET.get("name", "World")
+    greeting=request.GET.get("greeting","Bye")
+    return JsonResponse({"message": f"{greeting}, {name}!"})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_name), 
+    path('Bye/',Bye_name)
     # Example usage: /hello/?name=Bob
     # returns {"message": "Hello, Bob!"}
 ]
